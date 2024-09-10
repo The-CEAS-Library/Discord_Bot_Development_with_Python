de4956b520b92b2f102ad71320b67306d4c34de1
huuquangnhatnguyen <nguye2hq@mail.uc.edu>
2024-09-10 10:53:04 -0400

    exercises runthru and solution adjustment

--- Exercise Solutions/bot_code_solution.py
+++ Exercise Solutions/bot_code_solution.py
@@ -1,3 +1,4 @@
+from discord.ext import commands
 import discord
 
 # Assign the client variable to the result of discord.Client().
@@ -13,46 +14,43 @@ bot = discord.Bot(intents=intents)
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
 
-guild_ids = ['guild_id']
 
 # ping -- returns message "Pong!" to check the bot's online status.
-@bot.slash_command(guild_ids=guild_ids)
+@bot.slash_command(name="ping", description="Ping the bot")
 async def ping(ctx):
     await ctx.respond('Pong!')
 
 # hello - will return the string "Hello, user!" depending on user.
-@bot.slash_command(guild_ids=guild_ids)
+@bot.slash_command(name="hello")
 async def hello(ctx):
-    await ctx.respond('Hello!')
+    await ctx.respond('Hello User!')
+    
 
-# aboutme - will return the name and the pfp image of the user in an embed
-@bot.slash_command(guild_ids=guild_ids)
+#aboutme - will return the name and the pfp image of the user in an embed
+@bot.slash_command(name="about-me")
 async def aboutme(ctx):
-    embed=discord.Embed(title=ctx.author.name)
-    embed.set_thumbnail(url=ctx.author.avatar.url)
+    embed=discord.Embed(title=ctx.user.display_name)
+    embed.set_thumbnail(url=ctx.user.display_avatar)
     await ctx.respond(embed=embed)
 
 # help -- will direct message the user a help message.
-@bot.slash_command(guild_ids=guild_ids)
+@bot.slash_command(name="help", description='Type in the command that you want to know more about')
 async def help(ctx, cmd):
-    if cmd == 'help':
-        await ctx.respond('this message')
-    elif cmd == 'hello':
-        await ctx.respond('replies \'hello\' to your command')
-    elif cmd == 'ping':
-        await ctx.respond('replies \'Pong!\' to your command')
-    elif cmd == 'aboutme':
-        await ctx.respond('shows info about the user')
-    else:
-        await ctx.respond('command does not exist')
-
+    commands = {
+        'ping': ' is to check if the bot is online or not',
+        'hello': ' is to say hello to the bot',
+        'help': ' is to get help with the bot'
+    }
+    await ctx.send(cmd.upper() + commands[cmd])
+    
 # good bot
 @bot.event
 async def on_message(message):
-    # print(message)
     if message.content.lower() == 'good bot':
-        await message.add_reaction('❤️')
+      print('we re bt to run sth')
+      await message.add_reaction('💖')
 
 # This line assigns our client with the correct bot token, authenticating
-# that we have access to run our code on this bot user.
-bot.run('bot_token')
+# that we have access to run our code on this bot user. To do this, we just simply need to 
+# retrieve the token we store in the env
+bot.run(DISCORD_TOKEN)
