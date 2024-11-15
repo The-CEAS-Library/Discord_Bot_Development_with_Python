7a668d106738fc407b3da0dcd6566387e588b34b
Nhat HQ Nguyen <91711623+huuquangnhatnguyen@users.noreply.github.com>
2024-11-14 23:34:49 -0500

    Revert "Discord Bot updates"

--- bot_code_template.py
+++ bot_code_template.py
@@ -1,11 +1,10 @@
-from discord.ext import commands
 import discord
-import datetime
 
 # Assign the client variable to the result of discord.Client().
 intents = discord.Intents.default()
 intents.message_content = True
-bot = discord.Bot()
+
+bot = discord.Bot(intents=intents)
 
 # Define an async function under the client event parameter.
 # This function will print the bot user's client ID to our console
@@ -13,14 +12,32 @@ bot = discord.Bot()
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
+
+guild_ids = ['guild_id']
+
+# ping -- returns message "Pong!" to check the bot's online status.
+@bot.slash_command(guild_ids=guild_ids)
+async def ping(ctx):
+    await ctx.respond('Pong!')
+
+# hello - will return the string "Hello, user!" depending on user.
+@bot.slash_command(guild_ids=guild_ids)
+async def hello(ctx):
+    pass
+
+# help -- will direct message the user a help message.
+@bot.slash_command(guild_ids=guild_ids)
+async def help(ctx, cmd):
+    if cmd == 'help':
+        await ctx.respond('this message')
     
 
-@bot.slash_command(name="ping", description="Ping the bot")
-async def ping(ctx):
-    await ctx.respond('pong')
+# good bot
+@bot.event
+async def on_message(message):
+    if message.content.lower() == 'good bot':
+        pass
 
-@bot.slash_command(name="today", description="Command the bot to tell you the date")
-async def today(ctx):
-    today = datetime.date.today()
-    await ctx.send(today)
-
+# This line assigns our client with the correct bot token, authenticating
+# that we have access to run our code on this bot user.
+bot.run('bot_token')
